f687dcfb0e624be63704a532bb9ead9b6b2d3993
KAUSHAL ASHOK ZINE <32410264+kaushalzine@users.noreply.github.com>
2019-11-20 22:37:20 +0530

    Add files via upload

---
+++ Assignment1_1.py
@@ -0,0 +1,17 @@
+# 1.Write a program which contains one function named as Fun(). That function should display
+# “Hello from Fun” on console. 
+
+
+def fun():
+	print("Hello from Fun");
+
+fun()
+
+
+
+# Output-:
+
+
+# zine@DESKTOP-BU1PSC0 MINGW64 ~/Desktop/python/Ass1
+# $ python fun.py
+# Hello from Fun
